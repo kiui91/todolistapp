0dc793a598ad69125eafa6dc89f308ec8ca5f1f5
Kiui <107484409+kiui91@users.noreply.github.com>
2024-02-22 14:34:25 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
-import React, { useState, useEffect }  from "react";
-import TodoList from "./TodoList";
+import React from "react";
+import TodoList from "./TodoList"; // ToDoリストのロジックを含むコンポーネント
 import "./App.css";
-
-function ToDoApp() {
-    const [toDoList, setToDoList] = useState([]);
-
-    // 読み込み
-    useEffect(() => {
-        const savedToDoList = loadToDoList();
-        if (savedToDoList) {
-            setToDoList(savedToDoList);
-        }
-    }, []);
-
-    // 保存
-    useEffect(() => {
-        saveToDoList(toDoList);
-    }, [toDoList]);
-
-    // ToDoリストをローカルストレージに保存
-    function saveToDoList(toDoList) {
-        localStorage.setItem('toDoList', JSON.stringify(toDoList));
-    }
-
-    // ローカルストレージからToDoリストを読み込む
-    function loadToDoList() {
-        const savedToDoList = localStorage.getItem('toDoList');
-        return savedToDoList ? JSON.parse(savedToDoList) : [];
-    }
 
 function App() {
   return (
     <div className="App">
-      <TodoList />
+      <TodoList /> // ToDoリストの管理と表示を担当
     </div>
   );
 }
